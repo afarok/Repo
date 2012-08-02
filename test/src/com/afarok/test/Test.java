10f2307831c08e73ecc435b844c4151cb5b4d8d1
Ashraf Farok <afarok@timbasoftware.com>
2012-08-01 20:00:45 -0400

    Test.java is changed for testing purposes. eclipse classpath and .project configuration is added

--- test/src/com/afarok/test/Test.java
+++ test/src/com/afarok/test/Test.java
@@ -3,5 +3,6 @@ package com.afarok.test;
 public class Test {
     public static void main(String[] ags) {
         System.out.println("This is nothing but a simple test.");
+        System.out.println("Testing Git Version Control System.");
     }
 }
